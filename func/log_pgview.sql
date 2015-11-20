c643127ccd23de9c19f0458d936034baccea8ca8
steverusso <steverusso602@gmail.com>
2015-11-20 14:04:01 -0500

    Added the parameters to the 'log_pgview' function declaration.

--- func/log_pgview.sql
+++ func/log_pgview.sql
@@ -1,6 +1,13 @@
 
 CREATE OR REPLACE FUNCTION log_pgview(
-
+	_product 			VARCHAR(8),
+	_language 			VARCHAR(8),
+	_app_name 			VARCHAR(16),
+	_app_code_name 		VARCHAR(16),
+	_platform 			VARCHAR(32),
+	_app_version 		VARCHAR(128),
+	_page 				VARCHAR(128),
+	_user_agent 		VARCHAR(255)
 )
 RETURNS INTEGER
 AS $$
